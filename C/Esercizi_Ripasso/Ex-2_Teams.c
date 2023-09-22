e468793598cb657a2b10b7dbac792a2143a697bc
Sebastian-js <sebastian.jusca@liceonewton.edu.it>
2023-09-22 21:02:52 +0200

    finished ex 2

--- C/Esercizi_Ripasso/Ex-2_Teams.c
+++ C/Esercizi_Ripasso/Ex-2_Teams.c
@@ -2,3 +2,20 @@
 //  Ex-2_Teams.c
 //  Created by Sebastian Jusca on 22/09/2023.
 //
+
+#include "stdio.h"
+
+int main(){
+  int n, n_disp = 1, n_pari = 0;
+
+  printf("Enter value: \n");
+  scanf("%d",&n);
+
+  for(int i = 0; i<n; i++){
+    printf("%d ", n_disp + n_pari);
+    n_pari+=2;
+    n_disp+=2;
+  }
+  
+  return 0;
+}
