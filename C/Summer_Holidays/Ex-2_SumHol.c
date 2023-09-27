4a01b2a6437ac301c282fc4533e506cd6d39880e
Jusca Sebastian <127689068+Sebastian-js@users.noreply.github.com>
2023-09-27 09:06:51 +0200

    Update Ex-2_SumHol.c

--- C/Summer_Holidays/Ex-2_SumHol.c
+++ C/Summer_Holidays/Ex-2_SumHol.c
@@ -1,8 +1,5 @@
 //
 //  Ex-2_HolSumm.c
-//  Exercise 2 of 9 Explained on Microsoft Teams Summer School Holidays
-//  09422f59f79c0d5fff0c609e948e2d7908ec6fdc
-//
 //  Created by Sebastian Jusca on 21/06/2023.
 //
 
